0276f25fd8421ef437e1652c1ac567d2ca9f0b81
Domício Medeiros <domicioam@gmail.com>
2021-08-21 09:42:26 +0100

    ...

--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -61,17 +61,14 @@ class HuffmanSuite extends munit.FunSuite:
     assertEquals(string2Chars("hello, world"), List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
   }
 
-
   test("make ordered leaf list for some frequency table (15pts)") {
     assertEquals(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))), List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
   }
-
 
   test("combine of some leaf list (15pts)") {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
     assertEquals(combine(leaflist), List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
   }
-
 
   test("decode and encode a very short text should be identity (10pts)") {
     new TestTrees:
