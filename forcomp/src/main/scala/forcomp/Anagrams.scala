928ad3aa29882cfa7b9e264553c3ace6bb739b36
Domício Medeiros <domicioam@gmail.com>
2021-09-07 21:15:47 +0100

    word occurences

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -35,7 +35,11 @@ object Anagrams extends AnagramsInterface:
    *
    *  Note: you must use `groupBy` to implement this method!
    */
-  def wordOccurrences(w: Word): Occurrences = ???
+  def wordOccurrences(w: Word): Occurrences = 
+    w.groupBy(c => c.toLower)
+      .map(m => (m._1, m._2.length))
+      .toList
+      .sortBy(c => c._1)
 
   /** Converts a sentence into its character occurrence list. */
   def sentenceOccurrences(s: Sentence): Occurrences = ???
